524ce2c401222c15c7b99c006a2d2e477a50b724
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2021-02-14 08:09:52 +0530

    checkRequired function for validating null or empty fieldvalues

--- form_validator/script.js
+++ form_validator/script.js
@@ -6,9 +6,11 @@ const password2 = document.getElementById('password2');
 
 // Show input error message
 function showError(input, message) {
+  // console.log(input, message);
   const formControl = input.parentElement;
   formControl.classList.add('error');
   const small = formControl.querySelector('small');
+  small.visible = true;
   small.innerText = message;
 }
 
@@ -18,8 +20,29 @@ function showSuccess(input) {
   formControl.classList.add('success');
 }
 
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    // console.log(input);
+    // console.log(input.value);
+    if (input === null || input.value.trim() === '') {
+      // console.log(input.value);
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Get capitalized field name
+function getFieldName(input) {
+  // console.log(input.id.charAt(0).toUpperCase() + input.id.slice(1));
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
 
   // if (username.value === '') {
   //   showError(username, 'Username is required');
